7eb148d180718a197f5c20542bef73fcb3b614cd
Aron Xu <aron@debian.org>
2012-02-05 13:07:03 +0800

    Correct syntax.

--- debian/variables.mk
+++ debian/variables.mk
@@ -5,26 +5,26 @@ CXXFLAGS += $(CPPFLAGS)
 
 DEB_BUILD_MULTIARCH=$(shell dpkg-architecture -qDEB_BUILD_MULTIARCH)
 
-GTK2_LIBDIR=$(shell if [ -f /usr/lib/pkgconfig/gtk+-2.0.pc ];
+GTK2_LIBDIR=$(shell if [ -f /usr/lib/pkgconfig/gtk+-2.0.pc ]; \
     then echo "usr\/lib"; \
     else echo "usr\/lib\/$(DEB_BUILD_MULTIARCH)";  fi)
 
-GTK2_MA=$(shell if [ -f /usr/lib/pkgconfig/gtk+-2.0.pc ];
+GTK2_MA=$(shell if [ -f /usr/lib/pkgconfig/gtk+-2.0.pc ]; \
     then echo 0; \
     else echo 1;  fi)
 
-GTK3_LIBDIR=$(shell if [ -f /usr/lib/pkgconfig/gtk+-3.0.pc ];
+GTK3_LIBDIR=$(shell if [ -f /usr/lib/pkgconfig/gtk+-3.0.pc ]; \
     then echo "usr\/lib"; \
     else echo "usr\/lib\/$(DEB_BUILD_MULTIARCH)";  fi)
 
-GTK3_MA=$(shell if [ -f /usr/lib/pkgconfig/gtk+-3.0.pc ];
+GTK3_MA=$(shell if [ -f /usr/lib/pkgconfig/gtk+-3.0.pc ]; \
     then echo 0; \
     else echo 1;  fi)
 
-QT4_LIBDIR=$(shell if [ -f /usr/lib/pkgconfig/gtk+-3.0.pc ];
+QT4_LIBDIR=$(shell if [ -f /usr/lib/pkgconfig/gtk+-3.0.pc ]; \
     then echo "usr\/lib"; \
     else echo "usr\/lib\/$(DEB_BUILD_MULTIARCH)";  fi)
 
-QT4_MA=$(shell if [ -f /usr/lib/pkgconfig/gtk+-3.0.pc ];
+QT4_MA=$(shell if [ -f /usr/lib/pkgconfig/gtk+-3.0.pc ]; \
     then echo 0; \
     else echo 1;  fi)
