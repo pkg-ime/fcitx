9adb3d393adbeb8f4b6db2a761b525fa53c613d1
Aron Xu <aron@debian.org>
2012-02-05 13:45:02 +0800

    Some corrections.

--- debian/control.mk
+++ debian/control.mk
@@ -1,17 +1,17 @@
 # Generate control files.
 #
-# Generate debian/control:
-
-cat debian/control.in > debian/control
-$(shell for i in GTK2_MA GTK3_MA QT4_MA; do \
-    if [ $$(i) = 1 ]; then \
-	sed -i 's/@$$(i)@/Multi-Arch: same/g' debian/control \
-    else sed -i '/@$$(i)@/d' debian/control fi )
-
-# Generate .install files:
-sed 's/@GTK2_LIBDIR@/$(GTK2_LIBDIR)/g' \
-    debian/fcitx-frontend-gtk2.install.in > debian/fcitx-frontend-gtk2.install
-sed 's/@GTK3_LIBDIR@/$(GTK3_LIBDIR)/g' \
-    debian/fcitx-frontend-gtk3.install.in > debian/fcitx-frontend-gtk3.install
-sed 's/@QT4_LIBDIR@/$(QT4_LIBDIR)/g' \
-    debian/fcitx-frontend-qt4.install.in > debian/fcitx-frontend-qt4.install
+gen_control:
+	cat debian/control.in > debian/control
+	$(shell for i in GTK2_MA GTK3_MA QT4_MA; do \
+        if [ $($i) = 1 ]; then \
+	    sed -i 's/@$($i)@/Multi-Arch: same/g' debian/control \
+	else sed -i '/@$($i)@/d' debian/control; fi; done )
+	sed 's/@GTK2_LIBDIR@/$(GTK2_LIBDIR)/g' \
+	    debian/fcitx-frontend-gtk2.install.in > \
+	    debian/fcitx-frontend-gtk2.install
+	sed 's/@GTK3_LIBDIR@/$(GTK3_LIBDIR)/g' \
+	    debian/fcitx-frontend-gtk3.install.in > \
+	    debian/fcitx-frontend-gtk3.install
+	sed 's/@QT4_LIBDIR@/$(QT4_LIBDIR)/g' \
+	    debian/fcitx-frontend-qt4.install.in > \
+	    debian/fcitx-frontend-qt4.install
